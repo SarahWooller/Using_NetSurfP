5cedd8427c9abae53751d3da1a28a1f5e13b8e88
Sarah Wooller <sarahwooller@Sarahs-MBP.home>
2017-05-13 20:40:26 +0100

    dev0.1

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Using_NetSurfP
-Enablers automated interaction with NetSurfP
+# Using_NetSurfP #
+
+Enablers for automated interaction with NetSurfP
+
+# Version 0.1 #
+
+Development only.
+
+# Contact details #
+
+Sarah Wooller
+
+s.k.wooller@sussex.ac.uk
---
+++ setup.py
@@ -0,0 +1,19 @@
+
+import setuptools
+from distutils.core import setup
+
+setup(
+      name='Using_NetSurfP',
+      packages=['Using_NetSurP'],
+      version='0.1',
+      description='Tools to help with NetSurfP',
+      author='Sarah Wooller',
+      author_email='s.k.wooller@sussex.ac.uk',
+      url='https://github.com/SarahWooller/Using_NetSurfP',
+      download_url=('https://github.com/SarahWooller/Using_NetSurfP/archive/0.1.tar.gz'),
+      keywords=['ENST', 'NetSurfP', 'bioinformatics', 'Uniprot', 'fastas'],
+      classifiers=[],
+      setup_requires=[],
+      tests_require=[],
+      )
+
